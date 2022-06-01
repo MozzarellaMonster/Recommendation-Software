65419c06e048370acf20084508ce46fcf38dc9e4
Mozz <mozzarellamonster@users.noreply.github.com>
2022-06-01 01:19:43 -0600

    Added movies data file.

---
+++ movies_data.py
@@ -0,0 +1,8 @@
+movie_subgenres = {"found footage": ["Paranormal Activity", "REC", "The Blair Witch Project", "Cloverfield", "Creep", "Trollhunter", "Willow Creek", "V/H/S"],
+                   "comedy": ["Shaun of the Dead", "Tremors", "Ready or Not", "Freaky", "Fright Night", "Tucker & Dale vs Evil", "Gremlins", "Happy Death Day"],
+                   "slasher": ["Friday the 13th", "Halloween", "The Texas Chainsaw Massacre", "Child's Play"],
+                   "body horror": ["The Fly", "Tusk", "Eraserhead", "The Human Centipede", "The Thing", "Hellraiser", "Slither"],
+                   "supernatural": ["The Others", "Insidious", "The Conjuring", ""],
+                   "psychological": [""],
+                   "science fiction": ["Alien", "Predator", "The Fly", "The Blob", "The Thing", "Dark Skies", "Apollo 18", "Phoenix Forgotten", "Species", "Life", "Annihilation"],
+                   "monster": []}
