fc86f68c5db6769abe74ade12c5da1773156e48f
Mozz <mozzarellamonster@users.noreply.github.com>
2022-06-02 03:10:40 -0600

    Basic structure of recommendation software function added.

--- recommendation_software.py
+++ recommendation_software.py
@@ -1,5 +1,24 @@
 #Program created by Night Librarian/Mozzarella Monster
-from treenode import *
+from movies_data import *
+from quicksort import *
+from maxheap import MaxHeap
 
 def recommend():
-    pass
+    print("Welcome to my horror movie recommendation software!")
+    print("Here are the different tags you can use for the type of film you wish to view.")
+    print(tags)
+
+    user_tags = input("Please type in the tags you want your movie to have, separated by spaces: ")
+    user_tags_list = user_tags.split()
+    found_tags = []
+    
+    for tag in user_tags_list:
+        for movie in movies:
+            if tag in movies[movie]:
+                if tag not in found_tags:
+                    found_tags.append(tag)
+    print("The found tags are: " + str(found_tags))
+
+
+
+recommend()
