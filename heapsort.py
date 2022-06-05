83eb74680f5682a2049db9a6ece8fee5488a13bc
Mozz <mozzarellamonster@users.noreply.github.com>
2022-06-05 01:24:30 -0600

    Minor update.

--- heapsort.py
+++ heapsort.py
@@ -4,8 +4,8 @@ from maxheap import MaxHeap
 def heapsort(lst):
     sort = []
     max_heap = MaxHeap()
-    for idx in lst:
-        max_heap.add(idx)
+    for movie_tuple in lst:
+        max_heap.add(movie_tuple)
     while max_heap.count > 0:
         max_value = max_heap.retrieve_max()
         sort.insert(0, max_value)
