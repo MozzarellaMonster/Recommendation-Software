69a446865ad15f08a346fd8e1c6928adc7185c66
Mozz <mozzarellamonster@users.noreply.github.com>
2022-06-05 01:22:30 -0600

    MaxHeap class updated to better suit program's needs.

--- maxheap.py
+++ maxheap.py
@@ -4,37 +4,37 @@ class MaxHeap:
         self.heap_list = [None]
         self.count = 0
     
-    def parent_idx(self, idx):
-        return idx//2
+    def parent_idx(self, index):
+        return index//2
     
-    def left_child_idx(self, idx):
-        return idx * 2
+    def left_child_idx(self, index):
+        return index * 2
     
-    def right_child_idx(self, idx):
-        return idx * 2 + 1
+    def right_child_idx(self, index):
+        return index * 2 + 1
     
-    def child_present(self, idx):
-        return self.left_child_idx(idx) <= self.count
+    def child_present(self, index):
+        return self.left_child_idx(index) <= self.count
     
-    def add(self, element):
+    def add(self, movie_tuple):
         self.count += 1
-        print("Adding: {0} to {1}".format(element, self.heap_list))
-        self.heap_list.append(element)
+        print("Adding: {0} to {1}".format(movie_tuple, self.heap_list))
+        self.heap_list.append(movie_tuple)
         self.heapify_up()
     
     def heapify_up(self):
         print("Heapifying up")
-        idx = self.count
-        while self.parent_idx(idx) > 0:
-            child = self.heap_list[idx]
-            parent = self.heap_list[self.parent_idx(idx)]
-            if parent < child:
+        index = self.count
+        while self.parent_idx(index) > 0:
+            child = self.heap_list[index]
+            parent = self.heap_list[self.parent_idx(index)]
+            if parent[1] < child[1]:
                 print("swapping {0} with {1}".format(parent, child))
-                self.heap_list[idx] = parent
-                self.heap_list[self.parent_idx(idx)] = child
-            idx = self.parent_idx(idx)
-        print("Heap Restored {0}".format(self.heap_list))
-    
+                self.heap_list[index] = parent
+                self.heap_list[self.parent_idx(index)] = child
+            index = self.parent_idx(index)
+        parent("Heap restored {0}".format(self.heap_list))
+
     def retrieve_max(self):
         if self.count == 0:
             print("No items in heap")
@@ -47,28 +47,31 @@ class MaxHeap:
         print("Last element moved to first: {0}".format(self.heap_list))
         self.heapify_down()
         return max_value
-
+    
     def heapify_down(self):
-        idx = 1
-        while self.child_present(idx):
+        index = 1
+        while self.child_present(index):
             print("Heapifying down!")
-            larger_child_idx = self.get_larger_child_idx(idx)
-            child = self.heap_list[larger_child_idx]
-            parent = self.heap_list[idx]
-            if parent > child:
-                self.heap_list[idx] = child
-                self.heap_list[larger_child_idx] = parent
-            idx = larger_child_idx
+            larger_child_index = self.get_larger_child_idx(index)
+            child = self.heap_list[larger_child_index]
+            parent = self.heap_list[index]
+            if parent < child:
+                self.heap_list[index] = child
+                self.heap_list[larger_child_index] = parent
+            index = larger_child_index
         print("HEAP RESTORED! {0}".format(self.heap_list))
-        print("")
-
-    def get_larger_child_idx(self, idx):
-        if self.right_child_idx(idx) > self.count:
+    
+    def get_larger_child_idx(self, index):
+        if self.right_child_idx(index) > self.count:
             print("There is only a left child")
-            return self.left_child_idx(idx)
+            return self.left_child_idx(index)
         else:
-            left_child = self.heap_list(self.left_child_idx(idx))
-            right_child = self.heap_list(self.right_child_idx(idx))
-            if left_child > right_child:
+            left_child = self.heap_list[self.left_child_idx(index)]
+            right_child = self.heap_list[self.right_child_idx(index)]
+
+            if left_child[1] > right_child[1]:
                 print("Left child " + str(left_child) + " is larger than right child " + str(right_child))
-                return self.right_child_idx(idx)
+                return self.left_child_idx(index)
+            else:
+                print("Right child " + str(right_child) + " is larger than left child " + str(left_child))
+                return self.right_child_idx(index)
