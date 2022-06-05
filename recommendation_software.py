36c93334a29b972dfc037c7f40064c59743a54cf
Mozz <mozzarellamonster@users.noreply.github.com>
2022-06-05 02:49:26 -0600

    Added two helper methods and made some adjustments.

--- recommendation_software.py
+++ recommendation_software.py
@@ -1,4 +1,5 @@
 #Program created by Night Librarian/Mozzarella Monster
+from multiprocessing.sharedctypes import Value
 from movies_data import tags, movies
 from heapsort import heapsort
 from maxheap import MaxHeap
@@ -35,11 +36,28 @@ def recommendations():
                 tag_count += 1
         rec_movies[movie] = tag_count
         tag_count = 0
+
+    rec_movies_tuples = make_tuples(rec_movies)
+    rec_top_three = top_three(rec_movies_tuples)
         
     print("Your tags: " + " / ".join(user_tags_list))
-    print("The movies with those tags are: " + ", ".join("{0} ({1})".format(key, value) for key, value in rec_movies.items()))
-    print()
-    print("Your movies as tuples: " + str(rec_movies.items()))
+    print("The movies with those tags are: " + ", ".join("{0} ({1})".format(key, value) for key, value in rec_movies.items()) + "\n")
+    print("Your top three movies are: " + ", ".join("{0} ({1})".format(key, value) for key, value in rec_top_three))
+
+def make_tuples(rec_movies):
+    # Takes a dictionary with movie names as keys and
+    # number of tags it satisfies as the value
+    return [(key, value) for key, value in rec_movies.items()]
+
+def top_three(rec_movies_tuples):
+    # Takes a list of tuples with movie names as the first index
+    # and number of tags that satisfy as the second
+    top_three_list = []
+    for i in range(3):
+        rec_movies_tuples = heapsort(rec_movies_tuples[i:])
+        top_three_list.append(rec_movies_tuples[0])
+    return top_three_list
+    #return [heapsort(rec_movies_tuples[i:])[0] for i in range(3)]
 
 def test():
     outliers = []
