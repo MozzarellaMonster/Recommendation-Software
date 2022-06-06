e4ab774831b2481a8b0ee92cc6f1459539b37e75
Mozz <mozzarellamonster@users.noreply.github.com>
2022-06-06 03:11:57 -0600

    Removed redundant method.

--- recommendation_software.py
+++ recommendation_software.py
@@ -21,7 +21,7 @@ def recommendations():
         for tag in tags:
             if tag.startswith(user_tag) and tag not in user_tags_list:
                 user_tags_list.append(tag)
-        print("The tags you have chosen so far: " + " / ".join(user_tags_list))
+        print("The tags you have chosen so far: " + " / ".join(user_tags_list) + "\n")
         
     print("Finding movies for you...\n")
 
@@ -36,9 +36,11 @@ def recommendations():
                 tag_count += 1
         rec_movies[movie] = tag_count
         tag_count = 0
+    
+    rec_movies = {key:value for key, value in rec_movies.items() if value > 1}
 
     rec_movies_tuples = make_tuples(rec_movies)
-    rec_top_three = top_three(rec_movies_tuples)
+    rec_top_three = heapsort(rec_movies_tuples)[:3]
         
     print("Your tags: " + " / ".join(user_tags_list))
     print("The movies with those tags are: " + ", ".join("{0} ({1})".format(key, value) for key, value in rec_movies.items()) + "\n")
@@ -48,16 +50,6 @@ def make_tuples(rec_movies):
     # Takes a dictionary with movie names as keys and
     # number of tags it satisfies as the value
     return [(key, value) for key, value in rec_movies.items()]
-
-def top_three(rec_movies_tuples):
-    # Takes a list of tuples with movie names as the first index
-    # and number of tags that satisfy as the second
-    top_three_list = []
-    for i in range(3):
-        rec_movies_tuples = heapsort(rec_movies_tuples[i:])
-        top_three_list.append(rec_movies_tuples[0])
-    return top_three_list
-    #return [heapsort(rec_movies_tuples[i:])[0] for i in range(3)]
 
 def test():
     outliers = []
